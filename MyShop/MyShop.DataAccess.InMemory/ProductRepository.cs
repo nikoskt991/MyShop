d0a28783a430a9ad328425ae74814ea9fa00bc05
Nikos Kotzaris <nikoskt991@gmail.com>
2020-07-20 20:56:52 +0100

    ModelsAndStorage development

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+using System.Xml.Serialization;
+
+namespace MyShop.DataAccess.InMemory
+{
+    class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if(products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+    }
+}
